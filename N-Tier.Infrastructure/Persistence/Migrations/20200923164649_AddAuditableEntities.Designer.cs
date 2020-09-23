92ac738f5fbb4045e011b3f8a2febdee4e690ec0
nuyonu <nuyonu@gmail.com>
2020-09-23 19:47:06 +0300

    added auditable entity + migration

--- N-Tier.Infrastructure/Persistence/Migrations/20200923164649_AddAuditableEntities.Designer.cs
+++ N-Tier.Infrastructure/Persistence/Migrations/20200923164649_AddAuditableEntities.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using N_Tier.Infrastructure.Persistence;
 
 namespace N_Tier.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200923164649_AddAuditableEntities")]
+    partial class AddAuditableEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ N-Tier.Infrastructure/Persistence/Migrations/20200923164649_AddAuditableEntities.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace N_Tier.Infrastructure.Persistence.Migrations
+{
+    public partial class AddAuditableEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CretatedOn",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "CretatedOn",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TodoLists",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "TodoItems",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "TodoLists");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "TodoItems");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CretatedOn",
+                table: "TodoLists",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CretatedOn",
+                table: "TodoItems",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
